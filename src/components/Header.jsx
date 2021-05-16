603fb1e8ba0769c878176b9f131470f056dd94e1
Илья <bothisav@gmail.com>
2021-05-16 09:58:24 +0300

    014

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-
+import s from'./Header.module.css'
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT-_mIfmXfxZydIr5sSyiVu6p_LMiG6L8ZDw&usqp=CAU' />
     </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
-
+import s from './Profile.module.css'
 const Profile = () => {
-  return <div className='content'>
+  return <div className={s.content}>
     <div>
       <img src='https://images.unsplash.com/photo-1506038634487-60a69ae4b7b1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' />
     </div>
-    <div>
+    <div className={s.item}>
       ava
     </div>
     <div>
