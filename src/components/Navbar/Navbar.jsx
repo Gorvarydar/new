9f4eb936e74a74e2074bb1edb685d206351f896b
Илья <bothisav@gmail.com>
2021-05-18 06:54:55 +0300

    15

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,9 +7,10 @@ const Navbar = () => {
     <div className={`${s.item} ${s.active}`}>
       <a>Messages</a>
     </div>
-    <div className={s.item}>
+   
+  
       <a>News</a>
-    </div>
+   
     <div className={s.item}>
       <a>Music</a>
     </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,7 +9,6 @@ const Profile = () => {
       ava
     </div>
       <MyPosts />
-      
   </div>
 }
 
